612fc08490976689aabd97a3785ff2f0d40e1966
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-25 03:27:52 +0100

    added  logic for selecting user

--- src/component/SelectedUser.jsx
+++ src/component/SelectedUser.jsx
@@ -1,11 +1,17 @@
 import React, { useEffect } from 'react'
 
-const SelectedUser = (props) => {
-  useEffect(() => {
-    console.log(props)
-  }, [props])
-
-  return <div>SelectedUser</div>
+const SelectedUser = ({ userData }) => {
+  const { id, company, title, duties, dates } = userData
+  return (
+    <>
+      <div key={id}>
+        <h1>{company}</h1>
+        <h2>{title}</h2>
+        <h3>{duties}</h3>
+        <h4>{dates}</h4>
+      </div>
+    </>
+  )
 }
 
 export default SelectedUser
